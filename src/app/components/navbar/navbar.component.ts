36832c2f969b2a4c6e17a26cb263a3a5028b3f31
Ferris Thomas <fsthomas@gmail.com>
2017-09-29 12:19:06 -0400

    Updates nav bar to be a bit smarter and to listen for changes to the user.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from '../../models/Employee';
+import {DataService} from '../../services/data.service';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+    public user: Employee;
 
-  ngOnInit() {
-  }
+    constructor(private dataService: DataService) {
+    }
+
+    clearCreator(e) {
+        this.dataService.updateCurrentUser(null);
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    ngOnInit() {
+        this.dataService.currentUser$
+            .subscribe(res => {
+                this.user = res;
+            });
+        this.dataService.getCurrentUser()
+            .subscribe(res => {
+                this.user = res;
+            });
+    }
 
 }
