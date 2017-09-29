194bf641dc481a6fd9015953fdced0e60bc86b9e
Ferris Thomas <fsthomas@gmail.com>
2017-09-29 12:20:07 -0400

    Big old refactor. I should be more granular in the commits. Best Practices FTW.

--- src/app/components/jobs/new/new.component.ts
+++ src/app/components/jobs/new/new.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {DataService} from '../../../services/data.service';
-import {Cookies} from '../../../utilties/cookies';
-import {Creator} from '../../../models/Creator';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Employee} from '../../../models/Employee';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
     selector: 'app-new',
@@ -11,46 +11,38 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 
 export class NewComponent implements OnInit {
-    cookie: Cookies;
+
+    currentUser: Employee;
+    dataLoaded = false;
     departmentList: object[] = [{id: '-1', name: '---'}];
-    hasSavedCreator = false;
-    hasLoaded = false;
-    hasTyped = false;
+    dueDate: Date;
+    employee: Employee;
+    employees: any[] = [];
+    filteredEmployeeList: Employee[];
     hasSelected = false;
-    creators: Creator[];
-    creator: Creator;
-    filteredEmployeeList: Creator[] = [];
+    hasTyped = false;
+    invalidDates: Array<Date>;
+    maxDate: Date;
+    minDate: Date;
     newJobForm: FormGroup;
     newUserForm: FormGroup;
-    dueDate: Date;
-    minDate: Date;
-    maxDate: Date;
-    invalidDates: Array<Date>;
 
-    constructor(public dataService: DataService,
-                public formBuilder: FormBuilder) {
-        this.cookie = new Cookies();
+    constructor(private dataService: DataService,
+                private formBuilder: FormBuilder) {
     }
 
     clearSavedCreator() {
-        this.cookie.deleteJobstartCreator();
-        this.newJobForm.controls['creator_name'].setValue(null);
-        this.newJobForm.controls['creator_id'].setValue(null);
-        this.newJobForm.controls['creator_department'].setValue(-1);
-        this.hasSavedCreator = false;
-    }
-
-    dataLoaded() {
-        this.hasLoaded = true;
+        this.dataService.updateCurrentUser(null);
     }
 
     filterEmployees() {
-        this.hasSavedCreator = false;
         const query = this.newJobForm.controls['creator_name'].value;
         if (query !== '') {
-            this.filteredEmployeeList = this.creators.filter(function (el) {
-                return el.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
-            }.bind(this));
+            this.filteredEmployeeList = this.employees
+                .filter(employee => {
+                    return employee.full_name.toLowerCase()
+                        .indexOf(query.toLowerCase()) > -1;
+                });
             this.hasTyped = true;
         } else {
             this.filteredEmployeeList = [];
@@ -90,32 +82,33 @@ export class NewComponent implements OnInit {
         });
     }
 
-    showUserModal() {
-        this.newJobForm.controls['creator_name'].setValue('');
-        this.filteredEmployeeList = [];
-        this.hasTyped = false;
-    }
-
     onDepartmentSelect(e) {
         console.log('onDepartmentSelect', e);
     }
 
     onSelectCreator(employee) {
-        this.creator = employee;
-        this.newJobForm.controls['creator_name'].setValue(employee.name);
+        this.employee = employee;
+        this.newJobForm.controls['creator_name'].setValue(employee.full_name);
+        this.newJobForm.controls['creator_id'].setValue(employee.employee_id);
         this.filteredEmployeeList = [];
         this.hasTyped = false;
-        this.hasSelected = this.hasSavedCreator = true;
+        this.hasSelected = true;
+
         this.dataService.getEmployee(employee.employee_id)
             .subscribe(res => {
-                const deptId: string = (res.employee.departments.length >= 1) ? res.employee.departments[0].department_id : '';
+                this.employee = res as Employee;
+                const deptId: string = (res['departments'].length >= 1) ? res['departments'][0].department_id : '';
                 const dept: object[] = (this.departmentList.filter(d => {
                     return d['id'] === deptId;
                 }));
                 if (dept.length >= 1) {
                     this.newJobForm.controls['creator_department'].setValue(dept[0]['id']);
                 }
-                this.cookie.setJobstartCreator(employee.name, employee.id);
+                this.dataService.updateCurrentUser(employee)
+                    .subscribe(res => {
+                        this.currentUser = res;
+                    });
+
             });
     }
 
@@ -125,26 +118,34 @@ export class NewComponent implements OnInit {
     }
 
     onSubmitUserForm(form: any): void {
+        console.error('onSubmitUserForm');
         this.dataService.addEmployee(form)
             .subscribe(res => {
                 if (res.error === 200) {
                     // Successfully added the User
-                    const $ = window['$'];
-                    const newCreatorName = res.user.first_name
+                    const id = res['user']['employee_id'];
+                    this.employees = [];
+                    this.dataService.getEmployees()
+                        .subscribe(res => {
+                            this.employees.push(res);
+                        });
+                    this.dataService.getEmployee(id)
+                        .subscribe(res=>{
+                            console.log(res);
+                        });
+
+                    /*const newCreatorName = res.user.first_name
                         + (res.user.nickname === '' ? '' : ' "' + res.user.nickname) + '" '
                         + res.user.last_name;
 
-                    this.creator = {
-                        id: res.user.id,
-                        employee_id: res.user.employee_id,
-                        name: newCreatorName
-                    };
+                    this.creator = {...res, full_name:newCreatorName};
 
-                    this.newJobForm.controls['creator_name'].setValue(this.creator.name);
+                    this.newJobForm.controls['creator_name'].setValue(this.creator.full_name);
                     this.newJobForm.controls['creator_id'].setValue(this.creator.employee_id);
-                    this.cookie.setJobstartCreator(this.creator.name, this.creator.employee_id);
+                    this.cookie.setJobstartCreator(this.creator);
 
                     $('#addCreatorModal div.modal-header button').click();
+                    */
                 } else {
                     alert('Error ' + res.error + ': ' + res.message);
                     this.newUserForm = this.getNewUserForm();
@@ -152,10 +153,13 @@ export class NewComponent implements OnInit {
             });
     }
 
-    ngOnInit() {
+    showUserModal() {
+        this.newJobForm.controls['creator_name'].setValue('');
+        this.filteredEmployeeList = [];
+        this.hasTyped = false;
+    }
 
-        const creator = this.cookie.getJobstartCreator();
-        const creatorDepartmentId = this.cookie.getCreatorDepartment();
+    ngOnInit() {
 
         let today = new Date();
         let month = today.getMonth();
@@ -188,47 +192,76 @@ export class NewComponent implements OnInit {
         this.dueDate.setMilliseconds(0);
 
         const invalidDate = new Date();
+
         invalidDate.setDate(today.getDate() - 1);
+
         this.invalidDates = [today, invalidDate];
 
         this.newJobForm = this.getNewJobForm();
         this.newUserForm = this.getNewUserForm();
 
-        if (creator) {
-            this.hasSavedCreator = true;
-            this.creator = creator;
-            this.newJobForm.controls['creator_name'].setValue(this.creator.name);
-            this.newJobForm.controls['creator_id'].setValue(this.creator.id);
+        this.dataService.getCurrentUser()
+            .subscribe(res => {
+                if(res){
+                    this.currentUser = res as Employee;
+                    this.newJobForm.controls['creator_name'].setValue(this.currentUser.full_name);
+                    this.newJobForm.controls['creator_id'].setValue(this.currentUser.employee_id);
+                    if(!this.currentUser.department){
+                        this.dataService.getEmployee(this.currentUser.employee_id)
+                            .subscribe(res=>{
+                                this.currentUser.department = res['department'];
+                                this.newJobForm.controls['creator_department'].setValue(this.currentUser['department']['department_id']);
+                                this.dataService.updateCurrentUser(this.currentUser);
+                            });
+                    }else{
+                        this.newJobForm.controls['creator_department'].setValue(this.currentUser['department']['department_id']);
+                    }
+                }
+
+            });
+
+        /*
+
+        console.log(user);
+        this.hasSavedCreator = true;
+        this.creator = user;
+        this.newJobForm.controls['creator_name'].setValue(this.creator.full_name);
+        this.newJobForm.controls['creator_id'].setValue(this.creator.id);
+
+        if (creatorDepartmentId && this.creator.employee_id) {
+        creatorDepartmentId = this.cookie.getCreatorDepartment();
+        this.newJobForm.controls['creator_department'].setValue(creatorDepartmentId.id);
         }
-        if (creatorDepartmentId) {
-            this.newJobForm.controls['creator_department'].setValue(creatorDepartmentId.id);
-        }
+        */
 
         this.dataService.getEmployees()
             .subscribe(res => {
-                this.creators = res.employees.map(e => {
-                    let name = e.first_name;
-                    name += (e.nickname === '' ? '' : ' "' + e.nickname + '"');
-                    name += ' ' + e.last_name;
-                    return {
-                        employee_id: e.employee_id,
-                        id: e.id,
-                        name: name
-                    };
-                });
-                this.dataLoaded();
+                this.employees.push(res);
+                this.dataLoaded = this.employees.length > 0 && this.departmentList.length > 0;
             });
+
         this.dataService.getDepartments()
             .subscribe(res => {
-                let departments: object[] = res.departments;
-                departments.unshift({id: -1, name: '---'});
-                this.departmentList = departments;
-                this.dataLoaded();
+                this.departmentList.push(res);
+                this.dataLoaded = this.employees.length > 0 && this.departmentList.length > 0;
             });
 
         this.newJobForm.valueChanges.subscribe(data => {
-            if (this.cookie.getCreatorDepartment() !== data.creator_department) {
-                this.cookie.setCreatorDepartment(data.creator_department);
+            // console.warn('this.newJobForm.valueChanges');
+            // console.log(data);
+            // TODO: Determine if this is necessary.
+        });
+
+        this.dataService.currentUser$.subscribe(res=>{
+            console.log('NewComponent::this.dataService.currentUser$', res);
+            if(res){
+                this.currentUser = res;
+            }else{
+                // Clearing it out.
+                this.employee = this.currentUser = res;
+                this.newJobForm.controls['creator_name'].setValue(null);
+                this.newJobForm.controls['creator_id'].setValue(null);
+                this.newJobForm.controls['creator_department'].setValue(-1);
             }
         });
     }
