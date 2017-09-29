360e55cfab6cb6a0bf5e22638bbba40eeb6cc449
Ferris Thomas <fsthomas@gmail.com>
2017-09-29 12:18:34 -0400

    Fleshes out employee details more. Might be able to do away with the Crator model.

--- src/app/models/Creator.ts
+++ src/app/models/Creator.ts
@@ -1,5 +1,11 @@
 export interface Creator {
     id: string,
     employee_id: string,
-    name: string
+    first_name: string,
+    nickname: string,
+    last_name: string,
+    full_name: string,
+    email: string,
+    admin: string,
+    deleted: string
 }
--- src/app/models/Employee.ts
+++ src/app/models/Employee.ts
@@ -1,9 +1,13 @@
 export interface Employee {
     id: string,
+    department: string,
+    departments: any[],
+    employee_id: string,
     guid: string,
     first_name: string,
     nickname: string,
     last_name: string,
+    full_name: string,
     phone: string,
     office_phone: string,
     email: string,
